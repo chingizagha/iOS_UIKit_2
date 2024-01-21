24d90863ea496baf9e2d45a152c6ee490a22cc80
chingizagha <cingiz167@gmail.com>
2024-01-21 13:48:13 +0400

    Update

---
+++ Project9/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Chingiz on 19.01.24.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
+
---
+++ Project9/Project7/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project7
+//
+//  Created by Chingiz on 19.01.24.
+//
+
+import Foundation
+
+struct Petitions: Codable{
+    var results: [Petition]
+}
