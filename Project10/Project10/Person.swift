cf5d6a7f24de69c5137c2ee779ec391c2232d3b6
chingizagha <cingiz167@gmail.com>
2024-01-21 21:22:15 +0400

    Update

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,21 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Chingiz on 21.01.24.
+//
+
+import UIKit
+
+class Person: NSObject {
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    	
+
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,20 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Chingiz on 21.01.24.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+    
+    
+    
+}
