eb2903c8cdcf668eb3477c1393724052ecaa50f1
chingizagha <cingiz167@gmail.com>
2024-01-24 18:54:53 +0400

    Update

--- Project14/Project14/WhackSlot.swift
+++ Project14/Project14/WhackSlot.swift
@@ -36,17 +36,44 @@ class WhackSlot: SKNode {
     func show(hideTime: Double){
         if isVisible {return}
         
+        charNode.xScale = 1
+        charNode.yScale = 1
+        
+        
         charNode.run(SKAction.moveBy(x: 0, y: 80, duration: 0.05))
         isVisible = true
         isHit = false
         
         if Int.random(in: 0...2) == 0 {
-            charNode.texture = SKTexture(imageNamed: "penguidGood")
+            charNode.texture = SKTexture(imageNamed: "penguinGood")
             charNode.name = "charFriend"
         } else {
             charNode.texture = SKTexture(imageNamed: "penguinEvil")
             charNode.name = "charEnemy"
         }
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + (hideTime * 3.5)) {
+            [weak self] in
+            self?.hide()
+        }
+    }
+    
+    func hide(){
+        if !isVisible {return}
+        
+        charNode.run(SKAction.moveBy(x: 0, y: -80, duration: 0.05))
+        isVisible = false
+    }
+    
+    func hit(){
+        isHit = true
+        
+        let delay = SKAction.wait(forDuration: 0.25)
+        let hide = SKAction.moveBy(x: 0, y: -80, duration: 0.5)
+        let notVisible = SKAction.run{ [weak self] in self?.isVisible = false }
+        
+        let sequence = SKAction.sequence([delay, hide, notVisible])
+        charNode.run(sequence)
     }
     
     
